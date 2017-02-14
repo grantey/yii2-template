80423b65017db0fdafa6a6372fa76fe85c740876
grantey <grantey@gmail.com>
2017-02-14 12:22:51 +0300

    main

---
+++ common/config/params.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'adminEmail' => 'bakulin_av@finansda.ru',
+    'supportEmail' => 'bakulin_av@finansda.ru',
+    'contacts' => ['bakulin_av@finansda.ru'],
+    'mainPhone' => '+7 (495) 212-18-08',
+    'user.passwordResetTokenExpire' => 3600,
+    
+    'newsShortTruncate' => 200,
+    'newsLongTruncate' => 400,
+];
